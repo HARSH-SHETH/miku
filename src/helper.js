b62140b1a3eca4f20e089901baa9067ab3a375c2
harsh <ycdtharsh@gmail.com>
2021-07-12 02:55:51 +0530

    feature updates
    added filter group support to !miku
    all mime types working in sendWaifu().
    moved filterGroups() function to /src/helper.js

---
+++ src/helper.js
@@ -0,0 +1,13 @@
+// CONTAINS HELPER FUNCTIONS REQUIRED BY OTHER FILES
+const _ = require('./globals');
+
+module.exports.filterGroups = function(groupChat){
+  let bool = false;
+  _.FILTER_GROUPS.forEach((groupName) => {
+    if(groupChat.name.startsWith(groupName)){
+      console.log(groupName,'$', groupChat.name);
+      bool = true;
+    }
+  });
+  return bool;
+}
--- src/waifu/waifu.js
+++ src/waifu/waifu.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
 const { MessageMedia } = require('whatsapp-web.js');
+const { filterGroups } = require('../helper');
 
 const categories = [
   'waifu', 'neko', 'shinobu', 'megumin', 'bully', 'cuddle', 'cry', 
@@ -11,6 +12,12 @@ const categories = [
 const urlPrefix = 'https://api.waifu.pics/sfw/'
 
 module.exports = async function sendWaifu(msg, client){
+  // NO WAIFUS FOR BLOCKED GROUPS
+  let chat = await msg.getChat();
+  if(filterGroups(chat)){
+    msg.reply('Command not available in this group');
+    return;
+  }
   // let randomCategory = categories[Math.floor(Math.random() * categories.length)];
   let randomCategory = 'waifu';
   let url = urlPrefix + randomCategory;
@@ -19,9 +26,9 @@ module.exports = async function sendWaifu(msg, client){
     let response = await axios.get(url);
     url = response.data.url;
     let img = await axios.get(url, { responseType: 'arraybuffer' });
-    let base64encodedImage = await Buffer.from(img.data, 'binary').toString('base64');
-    let mimeType = url.slice(url.lastIndexOf('.')); 
-    msg.reply(new MessageMedia('image/png', base64encodedImage, null));
+    let base64encodedImage = Buffer.from(img.data, 'binary').toString('base64');
+    let mimeType = 'image/' + url.slice(url.lastIndexOf('.')+1); 
+    msg.reply(new MessageMedia(mimeType, base64encodedImage, null));
   }catch(err){
     console.error(err);
   }

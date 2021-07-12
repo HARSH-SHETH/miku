080c262bc16e1039492584c35ae22944c1e92996
harsh <ycdtharsh@gmail.com>
2021-07-12 23:37:15 +0530

    Print categories for sfw and nsfw commands.
    send gif file for mimetype 'image/gif'

--- src/waifu/waifu.js
+++ src/waifu/waifu.js
@@ -8,8 +8,9 @@ const categories = {
     'hug', 'awoo', 'kiss', 'lick', 'pat', 'smug', 'bonk', 'yeet', 
     'blush', 'smile', 'wave', 'highfive', 'handhold', 'nom', 'bite', 
     'glomp', 'slap', 'kill', 'kick', 'happy', 'wink', 'poke', 'dance', 'cringe',
+    'random',
   ],
-  nsfw: ['waifu', 'neko', 'trap', 'blowjob'],
+  nsfw: ['waifu', 'neko', 'trap', 'blowjob', 'random'],
 }
 
 
@@ -24,9 +25,12 @@ module.exports = async function sendWaifu(msg){
   }
   let type = msg.body.split(' ')[1];
   let category = msg.body.split(' ')[2];
-  if(category === undefined){
+  if(category === 'random'){
     // GET A RANDOM CATEGORY OF TYPE ${TYPE}
     category = categories[type][Math.floor(Math.random() * categories[type].length)];
+  }else if(category === undefined){
+    _sendCategories(msg, type);
+    return;
   }
   let url = `${urlPrefix}/${type}/${category}`;
   console.log(url);
@@ -36,8 +40,24 @@ module.exports = async function sendWaifu(msg){
     let img = await axios.get(url, { responseType: 'arraybuffer' });
     let base64encodedImage = Buffer.from(img.data, 'binary').toString('base64');
     let mimeType = 'image/' + url.slice(url.lastIndexOf('.')+1); 
-    msg.reply(new MessageMedia(mimeType, base64encodedImage, null));
+    console.log('mime type', mimeType);
+    let messageSendOptions = { caption: url };
+    if(mimeType === 'image/gif'){
+      messageSendOptions = { sendVideoAsGif: true, caption: url }
+      // mimeType = 'video/mp4';
+    }
+    msg.reply(new MessageMedia(mimeType, base64encodedImage), messageSendOptions);
   }catch(err){
     console.error(err);
   }
+}
+
+// SEND CATEGORIES ARRAY [SFW ONLY]
+function _sendCategories(msg, type){
+  let text = "```";
+  categories[type].forEach((category) => {
+    text += `[${category}], `
+  });
+  text += '```';
+  msg.reply(text);
 }

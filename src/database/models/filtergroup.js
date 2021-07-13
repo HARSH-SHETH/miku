a6bdf5207c2e94d57daf5ff1336bf1d3a16c71c8
harsh <ycdtharsh@gmail.com>
2021-07-13 22:38:54 +0530

    added MONGODBURL from remote mongodb instance
    removed manual creation and deletion of CSE DD Family group from
    filtergroup.js

--- src/database/models/filtergroup.js
+++ src/database/models/filtergroup.js
@@ -4,23 +4,4 @@ const filterGroupSchema = new mongoose.Schema({
   name: String,
 });
 
-const FilterGroupsModel = mongoose.model('FilterGroups', filterGroupSchema);
-
-// const newGroup = new FilterGroupsModel({
-//   name: 'CSE DD Family'
-// });
-
-// newGroup.save((err, group) => {
-//   if(err){
-//     console.log('error creating group');
-//   }else{
-//     console.log('group created successfully', group);
-//   }
-// });
-
-// FilterGroupsModel.findOneAndDelete({ name: 'CSE DD Family' }, (err, group) => {
-//   if(err) { console.log('error deleting group'); }
-//   else { console.log('deleted group', group); }
-// });
-
-module.exports = FilterGroupsModel;
+module.exports = mongoose.model('FilterGroups', filterGroupSchema);

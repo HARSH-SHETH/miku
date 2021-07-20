1c0bea13313dd6234f351e4ff7838c42d1b92901
harsh <ycdtharsh@gmail.com>
2021-07-21 00:31:39 +0530

    Feature: Stickers from url.
    Currently youtube urls are supported only.

--- src/sticker/sticker.js
+++ src/sticker/sticker.js
@@ -2,6 +2,8 @@
 const supportedDomainsArray = require('./supportedsite');
 const { MessageMedia } = require('whatsapp-web.js');
 const { spawn } = require('child_process');
+const fs = require('fs').promises;
+const crypto = require('crypto');
 
 // DEFAULT -SS VALUE TO USE IN FFMPEG COMMAND
 let seekstart = "00:00:00";
@@ -38,7 +40,7 @@ function _parseTimeStamp(msg){
   if(timestampRegex.test(timestamp)){
     seekstart = timestamp;
   }else{
-    msg.reply(`timestamp set to 00:00:00`);
+    // msg.reply(`timestamp set to 00:00:00`);
   }
 }
 
@@ -65,25 +67,24 @@ async function _parseBody(msg){
   // if(contains){
     let filename = await _extractVideoFromURL(extractedURL)
     if(filename){
-      console.log(__dirname, '/', filename);
       let media = MessageMedia.fromFilePath(`${__dirname}/${filename}`);
-      msg.reply(media, undefined, { sendMediaAsSticker: true, media });
+      msg.reply(media, undefined, { sendMediaAsSticker: true, media: media });
+      fs.unlink(`${__dirname}/${filename}`);
     }else{
-      // msg.reply('error occurred');
+      msg.reply(`error occured`);
     }
-
-  // }else{
-    // msg.reply('Domain Not Supported, See the list of supported domains here');
-  // }
 }
 
-function _extractVideoFromURL(url){
+async function _extractVideoFromURL(url){
   return new Promise((resolve) => {
+    let filename = crypto.randomBytes(6).readUIntLE(0, 6).toString(36);
+    console.log(filename);
     let ytdlArgs = [
       url,
+      '--no-continue',
       '--no-playlist',
-      '-f mp4',
-      '-o', 'test.%(ext)s',
+      '-f', 'mp4',
+      '-o', `${filename}.%(ext)s`,
       '--external-downloader', 'ffmpeg',
       '--external-downloader-args', 
       `-ss ${seekstart} -t 5`,
@@ -91,42 +92,64 @@ function _extractVideoFromURL(url){
 
     const yt = spawn('youtube-dl', ytdlArgs, { cwd: __dirname });
 
-    yt.stdout.on('data', (data) => {
+    yt.stderr.on('data', data => {
+      console.log(data.toString());
     });
 
-    yt.stderr.on('data', (data) => {
-      console.log('yt error', data.toString('utf-8'));
+    yt.stdout.on('data', data => {
+      console.log(data.toString());
     });
 
     yt.on('close', (code) => {
       console.log('yt exited with: ', code)
       if(code === 0){
-        resolve('test.mp4');
+        const ff = spawn('ffmpeg', [
+          '-nostdin',
+          // '-loglevel', 'error',
+          '-y', '-i', `${filename}.mp4`,
+          '-avoid_negative_ts', 'make_zero',
+          '-vcodec',
+          'libwebp',
+          '-lossless', '1',
+          '-vf',
+          // eslint-disable-next-line no-useless-escape
+          'scale=\'iw*min(300/iw\,300/ih)\':\'ih*min(300/iw\,300/ih)\',format=rgba,pad=300:300:\'(300-iw)/2\':\'(300-ih)/2\':\'#00000000\',setsar=1,fps=10',
+          '-loop',
+          '0',
+          '-ss',
+          '00:00:00.0',
+          '-t',
+          '00:00:05.0',
+          '-preset',
+          'default',
+          '-an',
+          '-vsync',
+          '0',
+          '-s',
+          '512:512',
+          '-f', 'webp',
+          `${filename}.webp`
+        ], { cwd: __dirname });
+
+        ff.stderr.on('data', (data) => {
+          console.log(`ffmpeg error: ${data.toString()}`);
+        })
+
+        ff.stdout.on('data', (data) => {
+          console.log(data.toString());
+        });
+
+        ff.on('close', async (code) => {
+          if(code === 0){
+            fs.unlink(`${__dirname}/${filename}.mp4`);
+            resolve(`${filename}.webp`);
+          }else{
+            resolve(null);
+          }
+        });
       }else{
         resolve(null);
       }
-      // if(code === 0){
-      //   const ff = spawn('ffmpeg', [
-      //     '-y', '-ss', seekstart, '-t', '5',
-      //     '-i', video_url.split('\n')[0],
-      //     // '-avoid_negative_ts', 'make_zero',
-      //     'test.mp4',
-      //   ]);
-
-      //   ff.stderr.on('data', data => {
-      //     console.log('ffmpeg err', data.toString('utf-8'));
-      //   });
-
-      //   ff.on('close', code => {
-      //     if(code === 0){
-      //       resolve('test.mp4');
-      //     }else{
-      //       resolve(null);
-      //     }
-      //   })
-      // }else{
-      //   resolve(null);
-      // }
     });
   });
 }
